0498245f667ae1ac5244e3b09931e5aec4056214
Samuel Rezende <sam_profissional@outlook.com>
2026-03-15 20:05:51 -0300

    refactor: auth_controller refactor for addressing best practices

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,25 +1,24 @@
 import type { HttpContext } from '@adonisjs/core/http'
-import vine from '@vinejs/vine'
-import AuthService from '../services/auth_service.ts'
+import { inject } from '@adonisjs/core'
+import { loginValidator } from '../validators/user.ts'
+import LoginUseCase from '../use-cases/auth/login_use_case.ts'
 
-const loginValidator = vine.create(
-  vine.object({
-    email: vine.string().email(),
-    password: vine.string(),
-  })
-)
-
+@inject()
 export default class AuthController {
+  constructor(private readonly loginUseCase: LoginUseCase) {}
+
   public async login({ request, response }: HttpContext) {
     const payload = await request.validateUsing(loginValidator)
 
     try {
-      const authService = new AuthService()
-      const authResult = await authService.authenticate(payload.email, payload.password)
+      const result = await this.loginUseCase.execute({
+        email: payload.email,
+        passwordPlain: payload.password,
+      })
 
-      return response.ok(authResult)
+      return response.ok(result)
     } catch (error) {
-      return response.status(error.status || 500).send({ message: error.message })
+      return response.unauthorized({ message: error.message })
     }
   }
 }
