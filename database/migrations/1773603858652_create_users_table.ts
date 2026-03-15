b95460487a21acec9800ef7adc4cb219d89ea03d
Samuel Rezende <sam_profissional@outlook.com>
2026-03-15 17:21:30 -0300

    chore: removes unused migrations and lint code format

--- database/migrations/1773603858652_create_users_table.ts
+++ database/migrations/1773603858652_create_users_table.ts
@@ -6,13 +6,9 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.uuid('id').primary()
-
       table.string('email', 255).notNullable().unique()
-
       table.string('password', 180).notNullable()
-
       table.enum('role', ['ADMIN', 'MANAGER', 'FINANCE', 'USER']).notNullable().defaultTo('USER')
-
       table.timestamp('created_at')
       table.timestamp('updated_at')
     })
