d481ff7af6362bdb1e666f3df11b202d8dadc4f1
Samuel Rezende <sam_profissional@outlook.com>
2026-03-15 22:24:35 -0300

    chore: product migration and model setup

---
+++ database/migrations/1773623767299_create_products_table.ts
@@ -0,0 +1,19 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'products'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+      table.string('name').notNullable()
+      table.integer('amount').notNullable()
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
