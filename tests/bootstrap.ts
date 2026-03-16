509a0dbcef1b1c45328951e9034d63d488633cc9
Samuel Rezende <sam_profissional@outlook.com>
2026-03-16 01:09:07 -0300

    test: setup proper environment for testing suites

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -38,8 +38,9 @@ export const plugins: Config['plugins'] = [
  * The setup functions are executed before all the tests
  * The teardown functions are executed after all the tests
  */
+
 export const runnerHooks: Required<Pick<Config, 'setup' | 'teardown'>> = {
-  setup: [],
+  setup: [() => testUtils.db().migrate()],
   teardown: [],
 }
 
